8f6d8a418cc8090c8f0b21a7510fbe43b5468abe
Rayan <rayanchecakarim@gmail.com>
2018-10-05 20:16:16 +0200

    Check random food

--- js/Food.js
+++ js/Food.js
@@ -1,15 +1,23 @@
 function Food(game){
 	this.game = game
-
 	this.food = [];
-	
+	1
 }
 
 Food.prototype.generateFood = function(){
-	
+	while (true){
+		
 	x = Math.floor((Math.random() * 25) + 1);
 	y = Math.floor((Math.random() * 25) + 1);	
+    e = {x:x,y:y,d:null};
+    if (!this.game.snake.isSnake(e)){
+		break;
+	}		
+	}
 	this.food.push({x:x,y:y,d:null});
+																						
+	
+	
 	
 }
 
